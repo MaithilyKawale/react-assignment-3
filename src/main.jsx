ccfc86fbee7c9a42538208d45d1931098ec21f90
MaithilyKawale <maithilyworkspace@gmail.com>
2025-11-20 12:53:18 +0530

    code corrected

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
